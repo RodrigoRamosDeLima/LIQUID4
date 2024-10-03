6d58c541124a4b822946900645c9963b5f4aeefa
RodrigoRamosDeLima <rodrigoramosdelima.dev@gmail.com>
2024-10-03 16:51:43 -0300

    pagina de login e cadastro liquid

---
+++ login/loginCadastroPage/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginCadastro from './components/LoginCadastro';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginCadastro />
+    </div>
+  );
+}
+
+export default App;
